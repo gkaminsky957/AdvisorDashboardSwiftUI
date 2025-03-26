396e5901be822d424bc9327e9b499ca47bddc8c3
Gennady Kaminsky <gennady@theleaguestock.com>
2025-03-26 17:33:51 -0500

    Initial Commit

---
+++ AdvisorDashboardSwiftUI/AdvisorDashboardSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvisorDashboardSwiftUIApp.swift
+//  AdvisorDashboardSwiftUI
+//
+//  Created by Gennady Kaminsky on 3/26/25.
+//
+
+import SwiftUI
+
+@main
+struct AdvisorDashboardSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
