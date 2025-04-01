809413cf0cb26dc0e0deba5c57c473cb38a98195
Gennady Kaminsky <gennady@theleaguestock.com>
2025-04-01 14:35:55 -0500

    Polished views

---
+++ AdvisorDashboardSwiftUI/CommonUI/ActivityIndicatorView.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityIndicatorView.swift
+//  AdvisorDashboardSwiftUI
+//
+//  Created by Gennady Kaminsky on 4/1/25.
+//
+
+import SwiftUI
+
+struct ActivityIndicatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityIndicatorView()
+}
